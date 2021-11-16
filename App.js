5ca2cf40f2d9c7bbabb5cdb9697ab29cec8277c0
최치원 <cw.choiit@choechiwon-ui-MacBookPro.local>
2021-11-16 23:09:24 +0900

    #02 Basic Settings

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const Text = styled.Text``;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Text>GONE CHAT APP</Text>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- README.md
+++ README.md
@@ -7,5 +7,26 @@
 - #01 Init
 
   ```bash
+  npx create-react-native-app <project-name> --use-npm
+
   npm install -D typescript @types/jest @types/react @types/react-native @types/react-test-renderer
+  ```
+
+- #02 Basic Settings
+
+  - React-Navigation
+
+  ```bash
+  npm install @react-navigation/native
+  expo install react-native-screens react-native-safe-area-context
+  npx pod-install
+  npm install @react-navigation/native-stack
+  npm install @react-navigation/bottom-tabs
+  ```
+
+  - styled-components
+
+  ```bash
+  npm install --save styled-components
+  npm install @types/styled-components @types/styled-components-react-native
   ```
